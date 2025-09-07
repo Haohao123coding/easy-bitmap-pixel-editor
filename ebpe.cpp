0bffef021eb74c7b301164790c192835fc53ce93
Haohao123coding <18217288892@163.com>
2025-09-07 17:14:48 +0800

    [feat] add open, cmd; remove rnd

--- ebpe.cpp
+++ ebpe.cpp
@@ -7,15 +7,6 @@
 #include<string>
 using namespace std;
 
-namespace rnd{
-	void setupSeed(){
-		srand(time(0));
-	}
-	int randInt(int a, int b){
-		return rand() % (b - a + 1) + a;
-	}
-};
-
 struct color{
 	int bit; // 24 or 32
 	int r, g, b, a;
@@ -42,14 +33,14 @@ struct fileHeader{
 
 struct infoHeader{
 	uint32_t biSize = 40;
-	int32_t	biWidth = 0;
-	int32_t	biHeight = 0;
+	int32_t biWidth = 0;
+	int32_t biHeight = 0;
 	uint16_t biPlanes = 1;
 	uint16_t biBitCount = 24;
 	uint32_t biCompression = 0;
 	uint32_t biSizeImage = 0;
-	int32_t	biXPelsPerMeter = 0;
-	int32_t	biYPelsPerMeter = 0;
+	int32_t biXPelsPerMeter = 0;
+	int32_t biYPelsPerMeter = 0;
 	uint32_t biClrUsed = 0;
 	uint32_t biClrImportant = 0;
 	infoHeader(){}
@@ -77,20 +68,75 @@ bmpFile curFile;
 string curFileName;
 
 namespace bmpOpr{
-	void saveBMP(){
+	void openBMP(string fileName){
 		// open file
-		ofstream file(curFileName, ios::binary);
+		ifstream file(fileName, ios::binary);
 		if(!file.is_open()){
 			cerr << "No file!" << endl;
 			return;
 		}
+		
+		// read headers
+	    file.read(reinterpret_cast<char*>(&curFile.fileh), sizeof(fileHeader));
+    	file.read(reinterpret_cast<char*>(&curFile.infoh), sizeof(infoHeader));
     	
-    	// calc
+    	// check
+    	if(curFile.fileh.bfType != 0x4D42){
+			cerr << "Not BMP file!" << endl;
+		}
+		if(
+			curFile.fileh.bfReserved1 != 0 ||
+			curFile.fileh.bfReserved2 != 0 ||
+			curFile.fileh.bfOffBits != 54
+		){ cerr << "No Support For This Format!"; return; }
+		if(
+			curFile.infoh.biSize != 40 ||
+			curFile.infoh.biWidth < 0 ||
+			curFile.infoh.biHeight < 0 ||
+			curFile.infoh.biPlanes != 1 ||
+			(
+				curFile.infoh.biBitCount != 24 &&
+				curFile.infoh.biBitCount != 32
+			) ||
+			curFile.infoh.biCompression != 0 ||
+			curFile.infoh.biXPelsPerMeter != 0 ||
+			curFile.infoh.biYPelsPerMeter != 0 ||
+			curFile.infoh.biClrUsed != 0 ||
+			curFile.infoh.biClrImportant != 0
+		){ cerr << "No Support For This Format!"; return; }
+		
+		// calc
     	int width = curFile.infoh.biWidth;
     	int height = curFile.infoh.biHeight;
     	int colorByte = curFile.infoh.biBitCount / 8;
     	int rowSize = (width * colorByte + 3) / 4 * 4;
     	int paddingSize = rowSize - width * colorByte;
+    	
+    	// read pixels
+    	for(int i = height - 1; i >= 0; i--){
+	        for(int j = 0; j < width; j++){
+	            file.read(reinterpret_cast<char*>(&curFile.bmap[i][j].b), 1);
+	            file.read(reinterpret_cast<char*>(&curFile.bmap[i][j].g), 1);
+	            file.read(reinterpret_cast<char*>(&curFile.bmap[i][j].r), 1);
+	            if(colorByte == 4){
+	                file.read(reinterpret_cast<char*>(&curFile.bmap[i][j].a), 1);
+	                curFile.bmap[i][j].bit = 32;
+	            }else{
+	                curFile.bmap[i][j].bit = 24;
+	            }
+	        }
+	        file.seekg(paddingSize, ios::cur);
+	    }
+	    
+	    // misc
+	    curFileName = fileName;
+	}
+	void calcSetBMP(){
+		// calc
+    	int width = curFile.infoh.biWidth;
+    	int height = curFile.infoh.biHeight;
+    	int colorByte = curFile.infoh.biBitCount / 8;
+    	int rowSize = (width * colorByte + 3) / 4 * 4;
     	
     	// set headers
     	curFile.infoh.biWidth = width;
@@ -98,6 +144,24 @@ namespace bmpOpr{
 		curFile.infoh.biSizeImage = rowSize * height;
 		curFile.fileh.bfSize =
 		sizeof(curFile.fileh) + sizeof(curFile.infoh) + curFile.infoh.biSizeImage;
+	}
+	void saveBMP(){
+		// open file
+		ofstream file(curFileName, ios::binary);
+		if(!file.is_open()){
+			cerr << "No file!" << endl;
+			return;
+		}
+		
+		// calc
+    	int width = curFile.infoh.biWidth;
+    	int height = curFile.infoh.biHeight;
+    	int colorByte = curFile.infoh.biBitCount / 8;
+    	int rowSize = (width * colorByte + 3) / 4 * 4;
+    	int paddingSize = rowSize - width * colorByte;
+    	
+    	// calc and set
+    	calcSetBMP();
 		
 		// write headers
 		file.write(reinterpret_cast<const char*>(&curFile.fileh), sizeof(fileHeader));
@@ -159,10 +223,121 @@ namespace bmpOpr{
 	}
 }
 
+namespace cmdOpr{
+	color analyse_color(string str){
+		int colorByte;
+		if(curFile.infoh.biBitCount == 24){
+			colorByte = 3;
+		}else{
+			colorByte = 4;
+		}
+		int len = str.length();
+		if(str[0] != '#' || (len != 7 && len != 9)){
+			// wrong format
+			return colorByte == 3 ? color(0, 0, 0) : color(0, 0, 0, 0);
+		}
+		for(int i = 1; i < len; i++){
+			if(!((str[i] >= '0' && str[i] <= '9') ||
+			(str[i] >= 'a' && str[i] <= 'f'))){
+				// wrong format
+				return colorByte == 3 ? color(0, 0, 0) : color(0, 0, 0, 0);
+			}
+		}
+		int r = stoi(str.substr(0, 2), nullptr, 16);
+        int g = stoi(str.substr(2, 2), nullptr, 16);
+        int b = stoi(str.substr(4, 2), nullptr, 16);
+        int a;
+        if(colorByte == 4) a = stoi(str.substr(6, 2), nullptr, 16);
+        if(colorByte == 3){
+			return color(r, g, b);
+		}else{
+			return color(r, g, b, a);
+		}
+	}
+	void loopTime(){
+		cout << curFileName << '<';
+		
+		// read commands
+		string cmd, tmp = "";
+		vector<string> cmds;
+		getline(cin, cmd);
+		int len = cmd.length(), wordCount = 0;
+		for(int i = 0; i < len; i++){
+			if(cmd[i] == ' '){
+				cmds.push_back(tmp);
+				tmp = "";
+			}else{
+				tmp += cmd[i];
+			}
+		}
+		wordCount = cmds.size();
+		
+		// analyse commands
+		if(cmds[0] == "set"){
+			if(wordCount != 3){
+				cerr << "Wrong Format!" << endl;
+			}else{
+				bool flagSuccess = true;
+				if(cmds[1] == "width"){
+					curFile.infoh.biWidth = stoi(cmds[2]);
+				}else if(cmds[1] == "height"){
+					curFile.infoh.biHeight = stoi(cmds[2]);
+				}else{
+					cerr << "Unknown argument!" << endl;
+					flagSuccess = false;
+				}
+				if(flagSuccess) bmpOpr::calcSetBMP();
+			}
+		}else if(cmds[0] == "get"){
+			if(wordCount != 2){
+				cerr << "Wrong Format!" << endl;
+			}else{
+				bool flagSuccess = true;
+				int answer;
+				if(cmds[1] == "width"){
+					answer = curFile.infoh.biWidth;
+				}else if(cmds[1] == "height"){
+					answer = curFile.infoh.biHeight;
+				}else{
+					cerr << "Unknown argument!" << endl;
+					flagSuccess = false;
+				}
+				if(flagSuccess) cout << cmds[1] << ": " << answer;
+			}
+		}else if(cmds[0] == "draw"){
+			if(wordCount < 2){
+				cerr << "Wrong Format!" << endl;
+			}
+			if(cmds[1] == "pixel"){
+				if(wordCount != 5){
+					cerr << "Wrong Format!" << endl;
+				}else{
+					int x = stoi(cmds[2]);
+					int y = stoi(cmds[3]);
+					color color_set = analyse_color(cmds[4]);
+					bmpOpr::editPixel(x, y, color_set);
+				}
+			}else if(cmds[1] == "rect"){
+				if(wordCount != 7){
+					cerr << "Wrong Format!" << endl;
+				}else{
+					int xf = stoi(cmds[2]);
+					int yf = stoi(cmds[3]);
+					int xl = stoi(cmds[4]);
+					int yl = stoi(cmds[5]);
+					color color_set = analyse_color(cmds[6]);
+					bmpOpr::drawRect(xf, yf, xl, yl, color_set);
+				}
+			}
+		}else{
+			cerr << "Unknown Command!" << endl;
+		}
+	}
+}
+
 int main(){
-	bmpOpr::genBMP(10, 10, color(0, 128, 255), "haha.bmp");
-	bmpOpr::editPixel(2, 2, color(255, 64, 0));
-	bmpOpr::drawRect(5, 5, 8, 7, color(64, 255, 32));
-	bmpOpr::saveBMP();
+	while(true){
+		cmdOpr::loopTime();
+	}
 	return 0;
 }
