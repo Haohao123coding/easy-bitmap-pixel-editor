fa95b9e4819a3aba43c700fccb498c0f09fdcc5b
Haohao123coding <18217288892@163.com>
2025-11-17 20:35:32 +0800

    code: add helpCommand.h

---
+++ src/helpCommand/helpCommand.h
@@ -0,0 +1,46 @@
+// *** EBPE ***
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2025 Haohao123coding
+
+#ifndef EASY_BITMAP_PIXEL_EDITOR_HELPCOMMAND_H
+#define EASY_BITMAP_PIXEL_EDITOR_HELPCOMMAND_H
+
+#include <map>
+#include <string>
+#include <vector>
+
+std::map<std::string, std::vector<std::string>> helpCommand = {
+    {
+        "gen", {
+            "gen <filename> <width> <height>",
+            "gen <filename> <width> <height> <fillingColor>"
+        }
+    },
+    {
+        "open", { "open <filename>" }
+    },
+    {
+        "save", { "save" }
+    },
+    {
+        "exit", { "exit" }
+    },
+    {
+        "set", { "set <item> <value>" }
+    },
+    {
+        "get", { "get <item>" }
+    },
+    {
+        "draw", {
+            "draw pixel <x> <y> <color>",
+            "draw rect <xFirst> <yFirst> <xLast> <yLast> <color>",
+            "draw rect <xFirst> <yFirst> <xLast> <yLast> <color> <borderPixelCount>"
+        }
+    },
+    {
+        "help", { "help <command>" }
+    }
+};
+
+#endif //EASY_BITMAP_PIXEL_EDITOR_HELPCOMMAND_H
